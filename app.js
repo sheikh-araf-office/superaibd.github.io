a0e97cf629c31ea485a9d443d17f573aec4be148
NewaysAI <sheikharaf.office@gmail.com>
2023-07-15 10:26:34 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// function addToCart(button) {
+//     // Get the cart icon element
+//     const cartIcon = document.getElementById('cart-icon');
+  
+//     // Get the current cart count
+//     let cartCount = parseInt(cartIcon.textContent);
+  
+//     // Update the cart count
+//     cartCount += 1;
+  
+//     // Update the cart icon text
+//     cartIcon.textContent = cartCount;
+//   }
+  
